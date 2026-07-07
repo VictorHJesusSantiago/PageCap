20050f12c2f1420c83c0cc192cec457514df180a
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-07-07 09:45:29 -0300

    [feat]  add notify function to electronAPI for desktop notifications

--- packages/electron/src/preload.ts
+++ packages/electron/src/preload.ts
@@ -3,5 +3,6 @@ import { contextBridge, ipcRenderer } from "electron";
 contextBridge.exposeInMainWorld("electronAPI", {
   openFolder: (path: string) => ipcRenderer.invoke("open-folder", path),
   chooseDirectory: () => ipcRenderer.invoke("choose-directory"),
+  notify: (title: string, body: string) => ipcRenderer.invoke("notify", title, body),
   isElectron: true,
 });
