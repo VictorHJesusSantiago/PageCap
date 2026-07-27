04e8b2da8311192a63ab5c6a81ff30a3457407b2
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-07-27 09:33:13 -0300

    feat: remove extract_documents from public API in extractors module

--- engine/extractors/__init__.py
+++ engine/extractors/__init__.py
@@ -1,11 +1,9 @@
 from .crawler import crawl_assets
 from .page import extract_page_pdf
 from .media import extract_media
-from .documents import extract_documents
 
 __all__ = [
     "crawl_assets",
     "extract_page_pdf",
     "extract_media",
-    "extract_documents",
 ]
